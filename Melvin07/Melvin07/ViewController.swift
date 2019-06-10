1de15c957828e2cd3862de6df85177183248a148
Raja Sadarangani <rpsadarangani@me.com>
2019-06-10 16:16:21 -0400

    Initialization
    Initialization of single view app

---
+++ Melvin07/Melvin07/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Melvin07
+//
+//  Created by Raja Prem Sadarangani on 6/10/19.
+//  Copyright © 2019 Raja Prem Sadarangani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.count + string.count - range.length
+        return newLength <= 10
+    }
+
+
+}
+
